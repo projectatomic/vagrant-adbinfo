0481589d55f930741e5d822953f4a415e8475d76
Navid Shaikh <nshaikh@redhat.com>
2015-11-06 22:31:10 +0530

    Updates the pscp command
     This patch updates the pscp command and removes unnecessary imports.

--- lib/command.rb
+++ lib/command.rb
@@ -1,5 +1,3 @@
-require 'open3'
-
 module OS
 
   def OS.windows?
@@ -34,37 +32,37 @@ module VagrantPlugins
 
           # First, get the TLS Certificates, if needed
           if !File.directory?(File.expand_path(".docker", secrets_path)) then
-              # Find the ssh information
-              hIP = machine.ssh_info[:host]
-              hport = machine.ssh_info[:port]
-              husername = machine.ssh_info[:username]
+            # Find the ssh information
+            hIP = machine.ssh_info[:host]
+            hport = machine.ssh_info[:port]
+            husername = machine.ssh_info[:username]
 
-	      if !OS.windows? then
-                hprivate_key_path = machine.ssh_info[:private_key_path][0]
-                # scp over the client side certs from guest to host machine
-                `scp -r -P #{hport} -o LogLevel=FATAL -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -i #{hprivate_key_path} #{husername}@#{hIP}:/home/vagrant/.docker #{secrets_path}`
-              else
-	        `pscp -r -P #{hport} -q -batch -pw vagrant #{husername}@#{hIP}:/home/vagrant/.docker #{secrets_path}`
-              end
-
+	          if !OS.windows? then
+              hprivate_key_path = machine.ssh_info[:private_key_path][0]
+              # scp over the client side certs from guest to host machine
+              `scp -r -P #{hport} -o LogLevel=FATAL -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -i #{hprivate_key_path} #{husername}@#{hIP}:/home/vagrant/.docker #{secrets_path}`
+            else
+              `pscp -r -P #{hport} -pw "vagrant" -p #{husername}@#{hIP}:/home/vagrant/.docker #{secrets_path}`
+            end
           end
+          # extending the path to include the .docker directory
+          secrets_path = File.expand_path(".docker", secrets_path)
+          
+          # Finds the host machine port forwarded from guest docker
+          port = machine.provider.capability(:forwarded_ports).key(2376)
+	  guest_ip = "127.0.0.1"
 
           # Print configuration information for accesing the docker daemon
-
-          # Finds the host machine port forwarded from guest docker
-           port = machine.provider.capability(:forwarded_ports).key(2376)
-	   guest_ip = "127.0.0.1"
-
           message =
-                <<-eos
+          <<-eos
 Set the following environment variables to enable access to the
 docker daemon running inside of the vagrant virtual machine:
 
 export DOCKER_HOST=tcp://#{guest_ip}:#{port}
-export DOCKER_CERT_PATH=#{secrets_path}/.docker
+export DOCKER_CERT_PATH=#{secrets_path}
 export DOCKER_TLS_VERIFY=1
 export DOCKER_MACHINE_NAME=#{machine.index_uuid[0..6]}
-                eos
+          eos
           @env.ui.info(message)
         end
       end
